0086ec97f8f466fb31f90ba8839e7fe90049ad82
a-singh09 <a_singh09@yahoo.com>
2023-09-10 20:07:15 +0530

    Import fix

--- furniture-store/app/about/page.tsx
+++ furniture-store/app/about/page.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Footer from '../components/Footer'
 import AboutContent from '../components/AboutContent'
 import TopHeaderButtonless from '../components/TopHeaderButtonless'
-import TopHeaderButtonless from '../components/TopHeaderButtonless'
 
 function About() {
   return (
