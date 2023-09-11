3794ea719a6faa18fe506316a525e427629f2bbc
a-singh09 <a_singh09@yahoo.com>
2023-09-11 22:51:02 +0530

    [FEAT] Added more testimonials

--- furniture-store/app/components/Testimonials.tsx
+++ furniture-store/app/components/Testimonials.tsx
@@ -62,7 +62,52 @@ export default function Testimonials() {
                 </blockquote>
               </div>
             </div>
-            
+            <div className="px-6 pt-16 pb-6 md:px-12 md:pb-10 relative rounded-lg shadow-sm bg-white dark:text-gray-100 dark:bg-gray-800">
+              <div className="absolute top-0 right-0 text-8xl mt-2 mr-5 text-gray-200 opacity-75 font-serif dark:text-gray-700/75">“</div>
+              <div className="relative">
+                <blockquote>
+                  <p className="font-medium text-lg leading-8 mb-5">
+                    I’ve been using pixelcave themes for years. The code is consistently high-quality and pleasant to work with, so I highly recommended them.
+                  </p>
+                  <footer className="flex items-center space-x-4">
+                    <a href="#" className="flex-none rounded-full overflow-hidden w-16 h-16 transition ease-out duration-150 border-2 border-gray-50 hover:border-white hover:shadow-md hover:scale-125 active:border-gray-50 active:shadow-sm active:scale-110 dark:border-gray-800 dark:shadow-gray-900">
+                      <img src="https://cdn.tailkit.com/media/placeholders/avatar-DLKR_x3T_7s-160x160.jpg" alt="Avatar Photo" />
+                    </a>
+                    <div>
+                      <a href="#" className="font-semibold text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300">
+                        Sue Keller
+                      </a>
+                      <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                        CEO and Founder
+                      </p>
+                    </div>
+                  </footer>
+                </blockquote>
+              </div>
+            </div>
+            <div className="px-6 pt-16 pb-6 md:px-12 md:pb-10 relative rounded-lg shadow-sm bg-white dark:text-gray-100 dark:bg-gray-800">
+              <div className="absolute top-0 right-0 text-8xl mt-2 mr-5 text-gray-200 opacity-75 font-serif dark:text-gray-700/75">“</div>
+              <div className="relative">
+                <blockquote>
+                  <p className="font-medium text-lg leading-8 mb-5">
+                    I’ve been using pixelcave themes for years. The code is consistently high-quality and pleasant to work with, so I highly recommended them.
+                  </p>
+                  <footer className="flex items-center space-x-4">
+                    <a href="#" className="flex-none rounded-full overflow-hidden w-16 h-16 transition ease-out duration-150 border-2 border-gray-50 hover:border-white hover:shadow-md hover:scale-125 active:border-gray-50 active:shadow-sm active:scale-110 dark:border-gray-800 dark:shadow-gray-900">
+                      <img src="https://cdn.tailkit.com/media/placeholders/avatar-euZ2n8dGUcQ-160x160.jpg" alt="Avatar Photo" />
+                    </a>
+                    <div>
+                      <a href="#" className="font-semibold text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300">
+                        Alejandro Lee
+                      </a>
+                      <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                        Full Stack Developer
+                      </p>
+                    </div>
+                  </footer>
+                </blockquote>
+              </div>
+            </div>
           </div>
           {/* END Feedback */}
         </div>
