0995203238ed01c20fc5fe2a686d7ad7660711e2
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-10-05 12:14:57 +0530

    custom  currsor

--- furniture-store/app/components/CustomCursor.tsx
+++ furniture-store/app/components/CustomCursor.tsx
@@ -8,6 +8,7 @@ const CustomCursor: React.FC = () => {
   useEffect(() => {
     const cursordot = document.querySelector('#cursor-dot') as HTMLElement;
     const cursorOutline = document.querySelector('#cursor-outline') as HTMLElement;
+    const cursorOuterOutline = document.querySelector('#cursor-outer-outline') as HTMLElement;
 
     window.addEventListener('mousemove', function (e) {
       const posX = e.clientX;
@@ -22,13 +23,24 @@ const CustomCursor: React.FC = () => {
       cursorOutline.animate({
         left: `${posX}px`,
         top: `${posY}px`
+      }, { duration: 800, fill: "forwards" })
+
+      cursorOuterOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
       }, { duration: 500, fill: "forwards" })
+
     })
 
     // Hover an element
     function onMouseHover() {
       console.log("mousehover");
       gsap.to('.cursor-outline', {
+        scale: 4,
+        duration: 0.3,
+      })
+
+      gsap.to('.cursor-outer-outline', {
         scale: 2,
         duration: 0.3,
       })
@@ -41,6 +53,10 @@ const CustomCursor: React.FC = () => {
         duration: 0.3,
       })
 
+      gsap.to('.cursor-outer-outline', {
+        scale: 1,
+        duration: 0.3,
+      })
     }
 
     const hoverables = document.querySelectorAll('.hoverable');
@@ -54,6 +70,7 @@ const CustomCursor: React.FC = () => {
 
   return (
     <div>
+       <div id="cursor-outer-outline" className="cursor-outer-outline"></div>
       <div id="cursor-outline" className="cursor-outline"></div>
       <div id="cursor-dot" className="cursor-dot"></div>
     </div>
